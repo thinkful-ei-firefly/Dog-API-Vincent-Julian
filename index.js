ec84b5cd2e10702194ea0c6281145f4b9543e74c
vbc221 <vbc221@nyu.edu>
2019-07-18 14:09:38 -0500

    changes

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 'use strict';
 
-function getDogImage(numberOfDogs = 3) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
+
+
+
+function getDogImage(numberOfDogs) {
+  fetch(`https://dog.ceo/api/breed/hound/images
+`)
     .then(response => response.json())
     .then(responseJson =>
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results').html(`<h2>Look at these dogs!</h2>`);
+  $('.results-img').empty();
 
-  for (let dog of responseJson) {
-    $('.results').append(
-      `<img src="${dog}" class="results-img" width="200" height="auto">`);
-  }
-  //display the results section
-  $('.results').removeClass('hidden');
+  responseJson.message.forEach(imgUrl => {
+    $('.results-img').append(`<img src="${imgUrl}" class="results-img">`);
+
+  });
+
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let numOfDogs = $('input[name="numOfDogs"]').val();
+    let breedOfDog = $('input[name="breedOfDog"]').val();
     getDogImage(numOfDogs);
   });
 }
